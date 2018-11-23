06dd3dccf84bb6f6424c38ec6abf9c5f6d1795ac
ahmadsyahrul <ahmadsyahrull02@gmail.com>
2018-11-23 17:28:03 +0700

    setup index

--- index.php
+++ index.php
@@ -6,11 +6,10 @@ $request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 $uri = explode('/', $request_uri[0]);
 
 /*
-    jika urlnya sepertin ini "http://localhost/rest_jwt/v2/about" maka hasil endpoint nya seperti ini.. 
-    [ "", "rest_jwt", "v2", "about"]
+    "http://localhost/rest_jwt/v2/about"  
+    uri ==>  [ "", "rest_jwt", "v2", "about"]
 */
 
-//mainkan endpoint nya disini !!!
 switch ($uri[2]) {
     default: // default endpoint//
         $api = new Api;
